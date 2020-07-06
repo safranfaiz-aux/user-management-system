322358bffdf07eb96b1e56dec06c66b3c44c9aaa
safranfaiz-aux <safranfaiz-aux@auxenta.clm>
2020-07-06 10:27:38 +0530

    db connection completed

--- api/server/server.js
+++ api/server/server.js
@@ -1,10 +1,29 @@
+const mysql = require("mysql");
 const express = require('express');
-
-const PORT = process.env.PORT || 8585;
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.json());
+
+var mysqlConnection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root",
+    database:"user_management"
+});
+
+mysqlConnection.connect( (error)=>{
+    if(!error){
+        console.log("Connected : MYSQL");
+    }else{
+        console.log("Connection Failed : MYSQL");
+    }
+
+} );
+
+
 //start the server
-app.listen(PORT, () => {
+app.listen(8383, () => {
     console.log('app running localhost:8585')
 })
