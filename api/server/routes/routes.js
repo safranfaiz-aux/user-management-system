d3399fc884fe51dda9efc6e0c579288c3a368998
safranfaiz-aux <safranfaiz-aux@auxenta.clm>
2020-07-07 15:42:56 +0530

    db connection post and get created

--- api/server/routes/routes.js
+++ api/server/routes/routes.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const dbConfig = require("../db_config/connectionDB");
+const { request, response } = require('express');
 
 const Router = express.Router();
 
-Router.get("/", (reqest, response) => {
-    dbConfig.query("select * from users", (error, rows, fields) => {
+Router.get("/all", (request, response) => {
+    dbConfig.query("select * from users", (error, result) => {
         if (!error) {
-            response.send(rows)
+            response.json(result);
         } else {
-            console.log("query error : ", error)
+            console.log("fetch all data error : ", error)
         }
     });
+});
+
+Router.post("/addUser", (request, response) => {
+    console.log("request >>> ", request.body);
+    let fName = request.body.fName;
+    let lName = request.body.lName;
+    let email = request.body.email;
+    let password = request.body.password;
+    let status = "1";
+
+    // console.log("Insert Query => "+ "inset into users ( first_name, last_name, email, password, status) values (" + fName + "," + lName + "," + email + "," + password + "," + status + " )");
+
+    dbConfig.query("insert into users ( first_name, last_name, email, password, status) values ( '"+fName+"','"+lName+"','"+email+"','"+password+"','"+status+"')",
+        (err, result) => {
+            if (!err) {
+                result.status = "success"
+                response.json(result);
+            } else {
+                console.log("data insertion error : "+err)
+                response.json(err);
+            }
+        });
 });
 
 module.exports = Router;
--- api/server/server.js
+++ api/server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const usersRoutes = require("./routes/routes");
-const dbConfig = require("./db_config/connectionDB");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.use("/all",usersRoutes);
+//domains specifications
+app.use("/user",usersRoutes);
 
 //start the server
 app.listen(8383, () => {
